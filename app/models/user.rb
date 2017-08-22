e4620875b9016fbf77451604e7672a5b5e71218f
kruulik <karoliskrulis@gmail.com>
2017-08-22 09:53:44 -0400

    Add user, session, application controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,48 @@
 class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    generate_unique_session_token
+    save!
+    session_token
+  end
+
+  private
+
+  def ensure_session_token
+    generate_unique_session_token unless session_token
+  end
+
+  def new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def generate_unique_session_token
+    self.session_token = new_session_token
+    while User.find_by(session_token: session_token)
+      self.session_token = new_session_token
+    end
+    session_token
+  end
 end
