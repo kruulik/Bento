20820835630b2bb48d0569861451e48c13471fff
kruulik <karoliskrulis@gmail.com>
2017-08-22 11:22:51 -0400

    Add store and import react modules

--- frontend/bento.jsx
+++ frontend/bento.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/session_api_util';
 
+import configureStore from './store/store.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
 
-  window.signup = APIUtil.signup;
-  window.login = APIUtil.login;
-  window.logout = APIUtil.logout;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<h1>Bentöööö</h1>, root);
 });
