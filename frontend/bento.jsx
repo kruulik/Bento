bb8571bc25ebafe79ab2880b3f0f7f2451f2d518
kruulik <karoliskrulis@gmail.com>
2017-08-22 11:03:24 -0400

    Add util and session actions

--- frontend/bento.jsx
+++ frontend/bento.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  window.signup = APIUtil.signup;
+  window.login = APIUtil.login;
+  window.logout = APIUtil.logout;
+
   ReactDOM.render(<h1>Bentöööö</h1>, root);
 });
