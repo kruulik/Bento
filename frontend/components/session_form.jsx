1839ff1dd3c6af073593ab794bd1039d5d526e71
kruulik <karoliskrulis@gmail.com>
2017-08-23 12:38:36 -0400

    Add auth error handling

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
+
 
 class SessionForm extends React.Component {
 
@@ -30,34 +31,75 @@ class SessionForm extends React.Component {
   }
 
   render() {
-    const header = this.props.formType;
-    const errors = this.props.errors.responseJSON;
+    // const header = this.props.formType;
+    let header, back, button, errors;
+
+    if (this.props.formType === 'signup') {
+      header = 'Sign up to continue';
+      back = <div className="row"><span>Already have an account? </span><Link to="/login">Sign in</Link></div>;
+      button = 'Sign Up';
+    } else {
+      header = 'Sign up to continue';
+      back = <div className="row"><span>Not a member yet? </span><Link to="/signup">Create an account</Link></div>;
+      button = 'Sign In';
+    }
+
+    // const errors = this.props.errors.responseJSON;
+    // debugger
+    if (this.props.errors.responseJSON) {
+    errors = this.props.errors.responseJSON.map((err) => <li>{err}</li>);
+    }
+    // debugger
     const fields = (
       [
-      <label htmlFor="f_name">First Name</label>,
+      <fieldset className="names">
+        <div className="grid_1">
+          <input type="text" id="f_name" value={this.state.f_name}
+          placeholder="First name"
+          onChange={this.update('f_name')}/>
+          </div>
+        <div className="grid_1">
+          <input type="text" id="l_name" value={this.state.l_name}
+          placeholder="Last name" onChange={this.update('l_name')}/>
+        </div>
+      </fieldset>
 
-      <input type="text" id="f_name" value={this.state.f_name} onChange={this.update('f_name')}/>,
-
-      <label htmlFor="l_name">Last Name</label>,
-
-      <input type="text" id="l_name" value={this.state.l_name} onChange={this.update('l_name')}/>
       ]
     );
     return (
-      <div>
-        <h3>{header}</h3>
+      <div className="form-container">
+        <div className="page-action">
+          <h1>{header}</h1>
+        </div>
+        <div className="form-logo">
+          <span className="logo">
+            <Link to="/">Bentö</Link>
+          </span>
+        </div>
       {this.props.loggedIn ? <Redirect to='/'/> : <div></div>}
         <form onSubmit={this.handleSubmit}>
-          <h6>{errors}</h6>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" value={this.state.email} onChange={this.update('email')}/>
 
           {(this.props.formType === 'signup') ? fields : null }
+          <fieldset className="email">
+            <input className="grid" type="text" id="email" value={this.state.email}
+              placeholder="Email address" onChange={this.update('email')}/>
+          </fieldset>
 
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" value={this.state.password} onChange={ this.update('password') }/>
+          <fieldset className="pass">
+            <input className="grid" type="password" id="password"
+              placeholder="Password" value={this.state.password} onChange={ this.update('password') }/>
+          </fieldset>
 
-          <button>{this.props.formType}</button>
+          <div className="row">
+            <button className="button__container form-button">{button}</button>
+          </div>
+          <div className="errors">{errors}</div>
+
+          <div className="row">
+            {back}
+          </div>
+
+
         </form>
 
       </div>
