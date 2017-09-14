c1ffe7f507d72929bb8851315e7f1fb283db1096
kruulik <karoliskrulis@gmail.com>
2017-09-14 11:38:32 -0400

    Remove unused components

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,12 +5,7 @@ import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const createLogger = require('redux-logger');
-//   middlewares.push(createLogger());
-// }
 if (process.env.NODE_ENV !== 'production') {
-  console.log('not production');
   const logger = require('redux-logger');
   middlewares.push(logger);
 }
