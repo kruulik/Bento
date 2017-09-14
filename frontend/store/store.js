bfdda67a5d994001f4e4ee248194b895374784f6
kruulik <karoliskrulis@gmail.com>
2017-09-14 11:53:00 -0400

    Fized production store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
-const middlewares = [thunk];
+let middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  const logger = require('redux-logger');
+  const {logger} = require('redux-logger');
   middlewares.push(logger);
 }
 
@@ -14,7 +13,7 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
